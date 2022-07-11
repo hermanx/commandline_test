2a0438f791303eab7fdff33c9a7b3b8b0af9eb55
Herman Xiao <hermanx@gmail.com>
2022-07-11 14:30:38 -0700

    Changed : to ; for multiple library separator

--- bin/oid.bat
+++ bin/oid.bat
@@ -1,6 +1,5 @@
-@echo off
 
-set SCRIPT_DIR=$(dirname $0)
-set INPUT_FILE="%SCRIPT_DIR%"/../snmp.yaml
+set SCRIPT_DIR=%~dp0%
+set INPUT_FILE=%SCRIPT_DIR%..\snmp.yaml
 
-java -cp $SCRIPT_DIR/../lib/*:$SCRIPT_DIR/../lib/thirdparty/* com.bouff.oids.OidAppLauncher $INPUT_FILE
+java -cp %SCRIPT_DIR%..\lib\*;%SCRIPT_DIR%..\lib\thirdparty\* com.bouff.oids.OidAppLauncher %INPUT_FILE%
