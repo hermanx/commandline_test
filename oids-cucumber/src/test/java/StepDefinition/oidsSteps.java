21f7883b7d23851f680200cf45a87faef6adb55e
Herman Xiao <hermanx@gmail.com>
2022-07-12 07:59:21 -0700

    Added HTML Report to BDD rans

--- oids-cucumber/src/test/java/StepDefinition/oidsSteps.java
+++ oids-cucumber/src/test/java/StepDefinition/oidsSteps.java
@@ -3,6 +3,10 @@
  */
 package StepDefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 /**
  * @author hermanx
  *
@@ -26,104 +30,8 @@ public class oidsSteps {
 	    throw new io.cucumber.java.PendingException();
 	}
 
-	@When("I input an invalid SNMP trap @")
-	public void i_input_an_invalid_snmp_trap() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input an invalid SNMP trap {int}")
-	public void i_input_an_invalid_snmp_trap(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input an invalid SNMP trap .")
-	public void i_input_an_invalid_snmp_trap() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input an invalid SNMP trap !")
-	public void i_input_an_invalid_snmp_trap() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input an invalid SNMP trap {double}.{int}.")
-	public void i_input_an_invalid_snmp_trap(Double double1, Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input an invalid SNMP trap abc")
-	public void i_input_an_invalid_snmp_trap_abc() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input an invalid SNMP trap {int}&")
-	public void i_input_an_invalid_snmp_trap(Integer int1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input an invalid SNMP trap {double}*")
-	public void i_input_an_invalid_snmp_trap(Double double1) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input a SNMP trap {double}.{int}.{int}.{int}.{int}.{int}.{int}.{int}.{int} to the CLI utility")
-	public void i_input_a_snmp_trap_to_the_cli_utility(Double double1, Integer int1, Integer int2, Integer int3, Integer int4, Integer int5, Integer int6, Integer int7, Integer int8) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
 	@Then("I verify the trap is a successor of any prefix to be result true")
 	public void i_verify_the_trap_is_a_successor_of_any_prefix_to_be_result_true() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input a SNMP trap {double}.{int}.{int}.{int}.{int}.{int}.{int}.{int}.{int}.{int} to the CLI utility")
-	public void i_input_a_snmp_trap_to_the_cli_utility(Double double1, Integer int1, Integer int2, Integer int3, Integer int4, Integer int5, Integer int6, Integer int7, Integer int8, Integer int9) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input a SNMP trap {double}.{int}.{int}.{int}.{int}.{int}.{int}.{int}.{int}.{int}.{int} to the CLI utility")
-	public void i_input_a_snmp_trap_to_the_cli_utility(Double double1, Integer int1, Integer int2, Integer int3, Integer int4, Integer int5, Integer int6, Integer int7, Integer int8, Integer int9, Integer int10) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input a SNMP trap {double}.{int}.{int}.{int}.{int}.{int}.{int}.{int} to the CLI utility")
-	public void i_input_a_snmp_trap_to_the_cli_utility(Double double1, Integer int1, Integer int2, Integer int3, Integer int4, Integer int5, Integer int6, Integer int7) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("I verify the trap is a successor of any prefix to be result false")
-	public void i_verify_the_trap_is_a_successor_of_any_prefix_to_be_result_false() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input a SNMP trap {double}.{int}.{int}.{int}.{int}.{int}.{int} to the CLI utility")
-	public void i_input_a_snmp_trap_to_the_cli_utility(Double double1, Integer int1, Integer int2, Integer int3, Integer int4, Integer int5, Integer int6) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input a SNMP trap {double}.{int}.{int}.{int}.{int}.{int} to the CLI utility")
-	public void i_input_a_snmp_trap_to_the_cli_utility(Double double1, Integer int1, Integer int2, Integer int3, Integer int4, Integer int5) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("I input a SNMP trap {double}.{int}.{int}.{int}. to the CLI utility")
-	public void i_input_a_snmp_trap_to_the_cli_utility(Double double1, Integer int1, Integer int2, Integer int3) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
